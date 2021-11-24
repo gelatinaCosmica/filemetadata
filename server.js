1999670c1b608b5792d976a0ef81dc50e0ba7f99
gelatina <santiagozapatabohorquez@hotmail.com>
2021-11-24 18:37:09 -0500

    completed project

--- server.js
+++ server.js
@@ -1,17 +1,36 @@
-var express = require('express');
-var cors = require('cors');
 require('dotenv').config()
 
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose')
+const db = require('mongodb')
+const bodyParser = require("body-parser")
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
+
+
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get('/', function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const fileData = {
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  }
 
+  res.json(fileData)
+})
 
 
 const port = process.env.PORT || 3000;
